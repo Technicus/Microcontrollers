e76e052a02d35f722f9d6d1d368a8433802db592
Technicus <fgardner@madisoncollege.edu>
2015-10-02 09:37:42 +0000

    $@

--- Utility/syncgithub.sh
+++ Utility/syncgithub.sh
@@ -1,6 +1,70 @@
 #!/bin/bash
 
-cd ..
-git add .
-git commit -m 'synchronize with github'
-git push origin master
+push(){
+  cd ..
+  git add .
+  # echo "git commit -m $@"     # test commit message
+  git commit -m '$@'
+  git push origin master
+  echo
+  exit 1
+}
+
+checkParameters(){
+  if [[ "$1" < "1" ]]
+  then
+    echo "      Usage:"
+    echo "              Be sure to include a commit message!"
+    echo
+    exit 1
+  else
+    push "${@:2}"
+  fi
+}
+
+showopts () {
+  while getopts ":pq:" optname
+    do
+      case "$optname" in
+        "p")
+          echo "Option $optname is specified"
+          ;;
+        "q")
+          echo "Option $optname has value $OPTARG"
+          ;;
+        "?")
+          echo "Unknown option $OPTARG"
+          ;;
+        ":")
+          echo "No argument value for option $OPTARG"
+          ;;
+        *)
+        # Should not occur
+          echo "Unknown error while processing options"
+          ;;
+      esac
+    done
+  return $OPTIND
+}
+
+showargs () {
+  for p in "$@"
+    do
+      echo "[$p]"
+    done
+}
+
+argumentDescription(){
+  optinfo=$(showopts "$@")
+  argstart=$?
+  arginfo=$(showargs "${@:$argstart}")
+  echo "Arguments are:"
+  echo "$arginfo"
+  echo "Options are:"
+  echo "$optinfo"
+}
+
+echo
+checkParameters $# $@
+# argumentDescription
+# echo exit test
