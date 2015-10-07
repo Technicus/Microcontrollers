9b50eeebc00fb39b49e98e118395c4d4e539eda8
Technicus <fgardner@madisoncollege.edu>
2015-10-07 22:35:04 +0000

    CHALLENGE 1 WORKS cd /home/technician/Education/MATC/Programming/Microcontrollers/Utility cd /home/technician/Education/MATC/Programming/Microcontrollers/Utility

--- Code-Design/Code/Analog/AnalogToDigital_00.c
+++ Code-Design/Code/Analog/AnalogToDigital_00.c
@@ -1,7 +1,7 @@
 /* Initial lcd test
-   2015-.09.24
-   ../../designs/LCDpanel_1.design
-*/
+ *   2015-.09.24
+ *   ../../designs/LCDpanel_1.design
+ */
 
 #include "../Library/Library_18f4520.h"
 
@@ -11,39 +11,39 @@ float Voltage;
 
 #INT_AD
 void int_ad_isr(){
-   Voltage = (*ADRESL * Vres) - 3;
+  Voltage = (*ADRESL * Vres) - 3;
 }
 
 main(){
-
-   lcd_init();
-   //printf(lcd_putc, "\fHello \n:)");
-
-   ADCON0->ADON = 1;
-   ADCON0->CHSx = 0;
-   ADCON1->PCFGX = 8;
-
-   *TRISA = 0x01;  // set A0 as input
-
-   ADCON2->ADFM = 1; // right justify
-
-   // enable references
-   ADCON1->VCFG0 = 1;
-   ADCON1->VCFG1 = 1;
-
-
-   // interrupt setup
-   PIE1->ADIE = 1;
-   INTCON->GIE = 1;
-   INTCON->PEIE = 1;
-
-   while(1){
-
-      printf(lcd_putc, "\f %fV", Voltage);
-       
-      ADCON0->GODONE = 1;
-      
-      delay_ms(20);
-
-   };
+  
+  lcd_init();
+  //printf(lcd_putc, "\fHello \n:)");
+  
+  ADCON0->ADON = 1;
+  ADCON0->CHSx = 0;
+  ADCON1->PCFGX = 8;
+  
+  *TRISA = 0x01;  // set A0 as input
+  
+  ADCON2->ADFM = 1; // right justify
+  
+  // enable references
+  ADCON1->VCFG0 = 1;
+  ADCON1->VCFG1 = 1;
+  
+  
+  // interrupt setup
+  PIE1->ADIE = 1;
+  INTCON->GIE = 1;
+  INTCON->PEIE = 1;
+  
+  while(1){
+    
+    printf(lcd_putc, "\f %fV", Voltage);
+    
+    ADCON0->GODONE = 1;
+    
+    delay_ms(20);
+    
+  };
 }
--- Code-Design/Code/Challenges/Challenge_1.c
+++ Code-Design/Code/Challenges/Challenge_1.c
@@ -1,13 +1,14 @@
 /* Initial lcd test
-   2015-.09.24
-   ../../designs/LCDpanel_1.design
-*/
+ *   2015-.09.24
+ *   ../../designs/LCDpanel_1.design
+ */
 
 #include "../Library/Library_18f4520.h"
 
 // Function declerations
-int analogPercentage(float analogValue);
-int ananlogKeyDifference(char press, int currentNumber);
+unsigned int analogPercentage(float analogValue);
+unsigned int ananlogKeyDifference(unsigned int percentVoltageRange, unsigned int keypadNumber_2);
+unsigned int keypadNumberUpdate(char press, unsigned int keypadNumber_0);
 
 // Global Variables
 float voltageRange = 6.0;
@@ -17,88 +18,99 @@ float Voltage;
 
 #INT_AD
 void int_ad_isr(){
-   Voltage = (*ADRESL * Vres) - 3;
+  Voltage = (*ADRESL * Vres) - 3;
 }
 
 main(){
-   
-   unsigned int percentVoltageRange = 0;
-   unsigned int currentNumber = 0;
-   char press;
-   
-   lcd_init();
 
-   ADCON0->ADON = 1;
-   ADCON0->CHSx = 0;
-   ADCON1->PCFGX = 8;
+  unsigned int percentVoltageRange = 0;
+  unsigned int keypadNumber_0 = 0;
+  unsigned int keypadNumber_1 = 0;
+  unsigned int ananlogKeyNumber = 0;
+  char press;
 
-   *TRISA = 0x01;  // set A0 as input
+  lcd_init();
 
-   ADCON2->ADFM = 1; // right justify
+  ADCON0->ADON = 1;
+  ADCON0->CHSx = 0;
+  ADCON1->PCFGX = 8;
 
-   // enable references
-   ADCON1->VCFG0 = 1;
-   ADCON1->VCFG1 = 1;
+  *TRISA = 0x01;  // set A0 as input
+
+  ADCON2->ADFM = 1; // right justify
+
+  // enable references
+  ADCON1->VCFG0 = 1;
+  ADCON1->VCFG1 = 1;
 
 
-   // interrupt setup
-   PIE1->ADIE = 1;
-   INTCON->GIE = 1;
-   INTCON->PEIE = 1;
+  // interrupt setup
+  PIE1->ADIE = 1;
+  INTCON->GIE = 1;
+  INTCON->PEIE = 1;
 
-   *TRISB = 0xF0;
-   *TRISC = 0x00;
-   ADCON1->PCFGx = 0x0F;
-   INTCON2->RBPU = 0;
+  *TRISB = 0xF0;
+  *TRISC = 0x00;
+  ADCON1->PCFGx = 0x0F;
+  INTCON2->RBPU = 0;
 
-   while(1){
+  while(1){
 
-      ADCON0->GODONE = 1;
+    ADCON0->GODONE = 1;
 
-      press = keyPress();
+    press = keyPress();
 
-      percentVoltageRange = analogPercentage(Voltage);
-      
+    percentVoltageRange = analogPercentage(Voltage);
 
-      if( press != defaultPress){
-      
-            currentNumber = ananlogKeyDifference(press, currentNumber);
-            
-            printf(lcd_putc, "\fV:%03.4fV KP:%c\nP:%d\t\t\t\t%i", Voltage, press, percentVoltageRange, currentNumber);
-      }
-      
-      delay_ms(10);
-   }
+    if( press != defaultPress){
+      keypadNumber_1 = keypadNumberUpdate(press, keypadNumber_0);
+      keypadNumber_0 = keypadNumber_1;
+      //test
+      //printf(lcd_putc, "\fNumber = %d", keypadNumber_1);
+      //KeypadNumber_0 = ananlogKeyDifference(press, currentNumber);
+
+      ananlogKeyNumber = ananlogKeyDifference(percentVoltageRange, keypadNumber_1);
+    }
+
+    printf(lcd_putc, "\fV:%03.4fV KP:%c\nP:%d%%\tS:%d\tN:%d", Voltage, press, percentVoltageRange, ananlogKeyNumber, keypadNumber_1);
+
+    delay_ms(20);
+  }
 }
 
 // Function definitions
 
-int analogPercentage(float analogValue){
-      return(((analogValue+3)/voltageRange)*100);
+unsigned int analogPercentage(float analogValue){
+  return(((analogValue+3)/voltageRange)*100);
 }
 
-int ananlogKeyDifference(char press, int currentNumber){
-      int keypadNumber;
-      int newNumber;
-      keypadNumber = press-0x30;
-      newNumber = currentNumber;
-      /*if (keypadNumber >= 0 && keypadNumber <= 10){
-      
-            newNumber = (currentNumber*10)+keypadNumber;
-            
-            if (currentNumber >= 10){
-                  
-                  newNumber = (currentNumber*10^1)+keypadNumber;     
-            }
-            else{
-                  newNumber = currentNumber + keypadNumber;     
-            }
-      }
-      else{
-            newNumber = 0;
-      }*/
-      currentNumber = currentNumber + keypadNumber;
-      return(currentNumber);  
+// update keypad number
+unsigned int keypadNumberUpdate(char press, unsigned int keypadNumber_0){
+  unsigned int keypadNumber_1 = 0;
+  keypadNumber_1 =  press - 0x30;
+  if (keypadNumber_1 >= 0 && keypadNumber_1 <= 9){
+    if (keypadNumber_0 < 10){
+      keypadNumber_1 = (keypadNumber_0 * 10) + (keypadNumber_1);
+      return(keypadNumber_1);
+    }
+    else{
+      return(keypadNumber_1);
+    }
+  }
+  else{
+    return(0);
+  }
+}
+
+unsigned int ananlogKeyDifference(unsigned int percentVoltageRange, unsigned int keypadNumber_2){
+  unsigned int ananlogKeyNumber = 0;
+  if (percentVoltageRange > keypadNumber_2){
+    ananlogKeyNumber = percentVoltageRange - keypadNumber_2;
+  }
+  else{
+    ananlogKeyNumber = keypadNumber_2 - percentVoltageRange;
+  }
+  return(ananlogKeyNumber);
 }
 
 
