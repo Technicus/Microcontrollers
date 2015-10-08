e365a22fcf4963898b3bbe076b81c790f74ae603
Technicus <fgardner@madisoncollege.edu>
2015-10-08 22:04:16 +0000

    Test

--- Code-Design/Code/Challenges/Challenge_1.c
+++ Code-Design/Code/Challenges/Challenge_1.c
@@ -1,4 +1,4 @@
-/* 
+/*
  *   Challenge - 1
  *   2015.10.08
  *   ../../designs/LCDpanel_1.design
@@ -8,7 +8,7 @@
 #include "../Library/Library_18f4520.h"
 
 // Function declerations
-unsigned int analogPercentage(float analogValue); 
+unsigned int analogPercentage(float analogValue);
 unsigned int ananlogKeyDifference(unsigned int percentVoltageRange, unsigned int keypadNumber_2);
 unsigned int keypadNumberUpdate(char press, unsigned int keypadNumber_0);
 
@@ -27,7 +27,7 @@ void int_ad_isr(){
 // Main program!!!!
 int main(int argc, char *argv[]){
 
-  unsigned int percentVoltageRange = 0;  // Value holder for voltage percentage 
+  unsigned int percentVoltageRange = 0;  // Value holder for voltage percentage
   unsigned int keypadNumber_0 = 0;       // Value holder for first keypress
   unsigned int keypadNumber_1 = 0;       // Value holder for new keypress
   unsigned int ananlogKeyNumber = 0;     // Value holder for difference between percentage and 2 digit keypad number
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]){
   *TRISA = 0x01;      // Set A0 as input
   *TRISB = 0xF0;      // ???
   *TRISC = 0x00;      // ???
-  
+
   // Analog setup
   ADCON0->ADON = 1;     // Turn on the analog circuits
   ADCON0->CHSx = 0;     // Choose the pin for reading analog value
@@ -47,29 +47,29 @@ int main(int argc, char *argv[]){
   ADCON1->VCFG0 = 1;    // Enable references high
   ADCON1->VCFG1 = 1;    // Enable references low
   ADCON1->PCFGx = 0x0F; // ???
-  
+
   // Interrupt setup
   PIE1->ADIE = 1;       // ???
   INTCON->GIE = 1;      // ???
   INTCON->PEIE = 1;     // ???
   INTCON2->RBPU = 0;    // ???
-  
+
   // Main process loop
   while(1){
 
     ADCON0->GODONE = 1;  // ???
 
     // Get input from keypad and check if it is pressed, if pressed update keypadNumber_0 and keypadNumber_1
-    press = keyPress(); 
+    press = keyPress();
     if( press != defaultPress){
       keypadNumber_1 = keypadNumberUpdate(press, keypadNumber_0);
       keypadNumber_0 = keypadNumber_1;
     }
-    
+
     // Get percentage of voltage range and calculate the difference between voltage% and keypad number
     percentVoltageRange = analogPercentage(Voltage);
     ananlogKeyNumber = ananlogKeyDifference(percentVoltageRange, keypadNumber_1);
-    
+
     // Formate data and update the LCD
     printf(lcd_putc, "\fV:%03.4fV KP:%c\nP:%d%%\tD:%d\tN:%d", Voltage, press, percentVoltageRange, ananlogKeyNumber, keypadNumber_1);
 
